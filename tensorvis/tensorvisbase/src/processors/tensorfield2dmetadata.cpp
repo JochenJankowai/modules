16080ff9fae999c5c8514b5f971bddcf58f778a2
Jochen Jankowai <jochen.jankowai@liu.se>
2020-02-17 11:18:05 +0100

    TensorVis: Extensive rework changes

--- tensorvis/tensorvisbase/src/processors/tensorfield2dmetadata.cpp
+++ tensorvis/tensorvisbase/src/processors/tensorfield2dmetadata.cpp
@@ -27,6 +27,8 @@
  *
  *********************************************************************************/
 
+#pragma optimize("", off)
+
 #include <inviwo/tensorvisbase/processors/tensorfield2dmetadata.h>
 #include <inviwo/tensorvisbase/util/tensorutil.h>
 #include <inviwo/core/util/stringconversion.h>
@@ -130,3 +132,5 @@ void TensorField2DMetaData::process() {
 }
 
 }  // namespace inviwo
+
+#pragma optimize("", on)
